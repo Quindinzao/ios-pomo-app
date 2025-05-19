a65d7ebd7574383eb87a1a6b3a5e4344e1b556e7
Quindinzao <devjaodev@gmail.com>
2025-05-19 01:24:10 -0300

    change LaunchScreen file, remove Main storyboard and create MVVM folder

---
+++ Pomo App/Model/PomodoroState.swift
@@ -0,0 +1,13 @@
+//
+//  PomodoroState.swift
+//  Pomo App
+//
+//  Created by João Victor Fernandes on 18/05/25.
+//
+
+import Foundation
+
+enum PomodoroState {
+    case focus
+    case breakTime
+}
---
+++ Pomo App/View/PomodoroViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PomodoroViewController.swift
+//  Pomo App
+//
+//  Created by João Victor Fernandes on 18/05/25.
+//
+
+import UIKit
+
+class PomodoroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
